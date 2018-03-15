c787e367700908da1b0062f274cc3138020a9b31
Kitty Davies <kittyfdavies@gmail.com>
2018-03-15 23:07:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,8 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.count >= 3
     last = array.pop
-    array << "and"
+    array << "and "
     array = array.join(", ")
-    array << " "
     array << last
   end
 end
